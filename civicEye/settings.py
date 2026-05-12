f820aa431cad798e6ac9e282245addf6babe234f
rania <raniabelkhiri77@gmail.com>
2026-05-12 16:58:27 +0100

    fix STATIC_ROOT for production

--- civicEye/settings.py
+++ civicEye/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!dfzy^ul@4k^^pto-$_nzeyq!^5gti_wjku4g6=pcmn_mtuyo8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".onrender.com"]
 
 
 # Application definition
@@ -127,3 +127,5 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
